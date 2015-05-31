ff7d42af0bdc2f67d4558525b6f81b346c593637
Danelson Rosa <danelson.rosa@gmail.com>
2015-05-31 14:25:26 -0400

    Updated css files. Schema includes automatic record creation.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
-#require 'sinatra'
+require 'sinatra'
 require 'pg'
+require 'pry'
 
 DBNAME = 'pairpals'
 
@@ -11,8 +12,31 @@ def db
     yield(connection)
   rescue
     connection.close
-  end  
+  end
 end
 
-system("createdb #{DBNAME}")
-system("psql #{DBNAME} < schema.sql")
+def sql(statement, exec_type, array_items)
+  all = db_connection do |conn|
+    if exec_type == "exec"
+      conn.exec(statement)
+    else
+      conn.exec_params(statement, array_items)
+    end
+  end
+  all = all.to_a
+end
+
+# Uncomment to automatically create a local db
+# and populate with sample records
+
+# system("createdb #{DBNAME}")
+# system("psql #{DBNAME} < schema.sql")
+
+get "/" do
+  redirect "/pairings"
+end
+
+get "/pairings" do
+  erb :index
+
+end
