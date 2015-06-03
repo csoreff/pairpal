735754e0db2c4f42c4063b3132ac986d73e21518
kevin larrabee <larrabeat@gmail.com>
2015-06-03 16:53:46 -0400

    Add heroku db reset to script

--- db_reset.rb
+++ db_reset.rb
@@ -1,3 +1,5 @@
-system("dropdb pairpals")
-system("createdb pairpals")
-system("psql pairpals < schema.sql")
+# system("dropdb pairpals")
+# system("createdb pairpals")
+# system("psql pairpals < schema.sql")
+
+system("heroku pg:psql DATABASE_NAME < schema.sql")
---
+++ test.rb
@@ -0,0 +1,6 @@
+require_relative 'helpers'
+require_relative 'app'
+
+thing = pairings
+
+thing.each {|t| puts t}
